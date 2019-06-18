fa3f7ced77ec0d72d90dd1632584fa65ec6e7ed1
nwokiwu <51241053+nwokiwu@users.noreply.github.com>
2019-06-18 17:50:39 -0400

    updated files
    Updated and working on js file

--- js/script.js
+++ js/script.js
@@ -17,6 +17,8 @@ FSJS project 2 - List Filter and Pagination
    scoped to that function.
 ***/
 
+let studentsList = document.querySelectorAll[]; 
+const studentsPage = 10;
 
 
 
@@ -28,22 +30,71 @@ FSJS project 2 - List Filter and Pagination
      - Keep in mind that with a list of 54 students, the last page 
        will only display four.
      - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
+     - Remember that a function `parameter` goes in the parents when 
        you initially define the function, and it acts as a variable 
        or a placeholder to represent the actual function `argument` 
        that will be passed into the parens later when you call or 
        "invoke" the function 
 ***/
 
+const showPage = (List,Page) => {
+  
+   let studentsList = (studentsPage)  
+   
+   [{
+      studentNames: [ 'name', 'category' ],
+      page: 3,
+      pagination: 
+[{
+ 
 
-
+//some actions
+         
+   /*
+   Loop over items in the list parameter
+   -- If the index of a list item is >= the index of the first
+   item that should be shown on the page
+   -- && the list item index is <= the index of the last item
+   that should be shown on the page, show it
+   */
+  for (i = 0; i < x.length; i++)
 
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
 
-
+const appendPageLinks = (studentsList) => {
+   $(function() {
+      var html = '<div class ="new1" ><img id = "new2" onclick = "showPage(' + id + 
+          ');" class = "' + id + 
+          '" src="@Url.Content("~/Images/blankpageQ.jpg")" />' + 
+          i + '</div>');
+      $(".pageListQuiz")
+        .children('div')
+        .eq(numOfPages - 1)
+        .append(html);
+    
+      function showPage() {
+        //some actions
+      }
+    });
+  
+  
+  
+   /*
+   1. Determine how many pages are needed for the list by dividing the
+   total number of list items by the max number of items per page
+   2. Create a div, give it the “pagination” class, and append it to the .page div
+   3. Add a ul to the “pagination” div to store the pagination links
+   4. for every page, add li and a tags with the page number text
+   5. Add an event listener to each a tag. When they are clicked
+   call the showPage function to display the appropriate page
+   6. Loop over pagination links to remove active class from all links
+   7. Add the active class to the link that was just clicked. You can identify that
+   clicked link using event.target
+   */
+   }
 
 
 
