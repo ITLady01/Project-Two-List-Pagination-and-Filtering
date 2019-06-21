fc70a9fcd1f6f92273f95c69d8c5d7a4c9e1a503
nwokiwu <51241053+nwokiwu@users.noreply.github.com>
2019-06-21 09:40:53 -0400

    Updated Changes
    Changes have been updated

--- js/script.js
+++ js/script.js
@@ -1,39 +1,48 @@
-//FSJS project 2 - List Filter and Pagination******************************************/
-
-
+/******************************************
+Treehouse Techdegree:
+FSJS project 2 - List Filter and Pagination
+******************************************/
+   
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 
-
-/* Added the two global variables
-  First variable studentsList this will pull the student list from the HTML file(DOM element)
-  Then I added the queryselector this will query and search for any tag name with a class name that is called student-list.
-Second variable is set to */
-let studentsList = document.getElementsByClassName('students-item cf'); 
-//let searchResults = Array.prototype.slice.call(document.querySelectorAll(".student-list"));
-
-const studentsPages = 10;
-//const pageHeader = document.querySelector('.page-header');
-
-//const page = document.querySelector('.page');
-//console.log(studentsList);
-
-
-//Created a function that will show the page and list all ten students at one time and will hide the remaining student list
-const showPage = (list, page) => {
- //This will go for firstpage and will list 10 students
-   let indexStart = (page * studentsPages) - studentsPages;
-   let indexEnd = (page * studentsPages);
-
-  
-
-   //Select appropriate
-  
-  
-//some actions
-         
+/*** 
+   Add your global variables that store the DOM elements you will 
+   need to reference and/or manipulate. 
    
-  /*** Loop over items in the list parameter
+   But be mindful of which variables should be global and which 
+   should be locally scoped to one of the two main functions you're 
+   going to create. A good general rule of thumb is if the variable 
+   will only be used inside of a function, then it can be locally 
+   scoped to that function.
+***/
+
+let studentsList = document.querySelector('student-list'); 
+const studentsPages = 10;
+
+
+
+/*** 
+   Create the `showPage` function to hide all of the items in the 
+   list except for the ten you want to show.
+
+   Pro Tips: 
+     - Keep in mind that with a list of 54 students, the last page 
+       will only display four.
+     - Remember that the first student has an index of 0.
+     - Remember that a function `parameter` goes in the parens when 
+       you initially define the function, and it acts as a variable 
+       or a placeholder to represent the actual function `argument` 
+       that will be passed into the parens later when you call or 
+       "invoke" the function 
+***/
+
+const showPage = (list, page) => {
+
+    let indexStart = (page * studentsPages) - studentsPages;
+    let indexEnd = (page * studentsPages);
+
+    /*** Loop over items in the list parameter
    -- If the index of a list item is >= the index of the first
    item that should be shown on the page
    -- && the list item index is <= the index of the last item
@@ -41,56 +50,63 @@ const showPage = (list, page) => {
    
 
 for (let i = 1; i < studentsList.length; i++) {
-let li= list[i];
-if (i >= indexStart  && i< indexEnd) {
-   list[i].style.display = "block";// This will show the pages
-      } else { 
-      list[i].style.display = "none";//This will hide the students pages
-      }
-   }
+    let li= list[i];
+    if (i >= indexStart  && i< indexEnd) {
+       list[i].style.display = "block";// This will show the pages
+          } else { 
+          list[i].style.display = "none";//This will hide the students pages
+          }
+       }
+    
+    };
+    
+ 
+/*** 
+   Create the `appendPageLinks function` to generate, append, and add 
+   functionality to the pagination buttons.
+***/
 
-};
 //showPage(studentsList, 2)
    /*The`appendPageLinks function` will generate, append, and add 
    functionality to the pagination buttons.*/
 
    const appendPageLinks = (list) => {
-   let totalPages = Math.ceil(list.length/studentsPages);
-   const PageOfDivElement = document.querySelector('.page');
-   const paginationDiv = document.createElement('div');
+    let totalPages = Math.ceil(list.length/studentsPages);
+    const PageOfDivElement = document.querySelector('.page');
+    const paginationDiv = document.createElement('div');
+ // I am setting an attribute class to pagination
+ document.querySelector(".page").appendChild(paginationDiv)// Appending the child
+ paginatonDiv.setAttribute('class','pagination');//set the attribute to pagination
+ let ul= document.createElement('ul');//create an element
+ //pagination.classList.add('pagination');//create a classList and 
+ paginationDiv.appendChild(ul);
 
-   // I am setting an attribute class to pagination
-   document.querySelector(".page").appendChild(paginationDiv)// Appending the child
-   paginatonDiv.setAttribute('class','pagination');//set the attribute to pagination
-   let ul= document.createElement('ul');//create an element
-   //pagination.classList.add('pagination');//create a classList and 
-   paginationDiv.appendChild(ul);
+ for (let i = 1; i <= totalPages; i++){
+  let li = document.createElement('li');
+  let link = document.createElement('a');
+  link.setAttribute('class', 'link');
+  a.className = 'active';
+  let ActivePage = event.target.textContent;
+  link.innerHTML = i;
+  
+  link.addEventListener("click",(event) => {
+  const buttons = event.target;
+  const paginationLink = document.querySelectorAll('.paginationLi');
+  for( let i = 0; i, buttons.length; i ++){
+buttons[i].className ="none";
+  }
+  buttons.className ="active";
+});
+ }
+  
+  li.appendChild(link);
+  ul.appendChild(li);
+ }
+  showPage(studentsList, ActivePage);
+  appendPageLinks(studentsList);
+  
 
-   for (let i = 1; i <= totalPages; i++){
-    let li = document.createElement('li');
-    let link = document.createElement('a');
-    link.setAttribute('class', 'link');
-    let ActivePage = event.target.textContent;
-    link.innerHTML = i;
-    
-    link.addEventListener("click",(event) => {
-    const buttons = event.target;
-    const paginationLink = document.querySelectorAll('.paginationLi');
-    showPage(studentsList, ActivePage);
 
- 
-     
-    });
-    
-    li.appendChild(link);
-    ul.appendChild(li);
-   }
-    showPage(studentsList, ActivePage);
-    appendPageLinks(studentsList);
-}
-    //let buttons = document.querySelectorAll('a');
-    
-   
-    
 
-    
+
+// Remember to delete the comments that came with this file, and replace them with your own code comments.
