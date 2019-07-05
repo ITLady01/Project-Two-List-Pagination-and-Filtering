2d881c5bebcdb68ad1bce5ac78bb2fa1e77f60a6
nwokiwu <51241053+nwokiwu@users.noreply.github.com>
2019-07-05 13:41:51 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,13 +18,13 @@ document.querySelector('.page').appendChild(p);
 //This code has been tested and is working
 //The showPage function will show list and page
 const showPage = (list, page) => {
-//The index first page will start the page from 0-11 and etc.
-//The index last page will end the page 
+   //The index first page will start the page from 0-11 and etc.
+   //The index last page will end the page 
    let startIndex = (page * studentsPages) - studentsPages;
    let endIndex = page * studentsPages;
 
-   for(let i = 0; i < list.length; i++) {// The students List will be going through a loop 
-      if(i >= startIndex && i < endIndex) {
+   for (let i = 0; i < list.length; i++) {// The students List will be going through a loop 
+      if (i >= startIndex && i < endIndex) {
          list[i].style.display = 'block';// This will show the pages
       } else {
          list[i].style.display = 'none';//This will hide the students pages
@@ -33,9 +33,9 @@ const showPage = (list, page) => {
 }
 
 // showPage(studentsList, 2); Tested the code and called function and it displayed the ten students and hid the rest of the list.
- /*The `appendPageLinks function` will generate, append, and add functionality to the pagination buttons.*/
- const appendPageLinks = (list) => {
-   const paginationDiv= document.createElement('div');// Creating div and creating an element for div
+/*The `appendPageLinks function` will generate, append, and add functionality to the pagination buttons.*/
+const appendPageLinks = (list) => {
+   const paginationDiv = document.createElement('div');// Creating div and creating an element for div
    const PageOfDivElement = document.querySelector('.page');//The parent is PageOfDivElement and it will select the class name .page from the HTML file
    const ul = document.createElement('ul');//Creating an element for ul
    const AmtofPages = Math.ceil(list.length / 10);// This is calculating the total number of pages from the student list.
@@ -44,8 +44,8 @@ const showPage = (list, page) => {
    PageOfDivElement.appendChild(paginationDiv);// Appending the parent to the child (paginationDiv)
    paginationDiv.appendChild(ul);//Appending the child to (ul)
 
-   
-   for(let i = 0; i <AmtofPages; i++){//For loop to set the amount of pages to loop 10 students per page
+
+   for (let i = 0; i < AmtofPages; i++) {//For loop to set the amount of pages to loop 10 students per page
       const li = document.createElement('li');// Adding a <li> name tag 
       ul.appendChild(li);// Appending the child to the li
       const link = document.createElement('a');// Adding <a> name tag
@@ -60,17 +60,17 @@ const showPage = (list, page) => {
 
    ul.firstElementChild.firstElementChild.className = "active";//Set the first element child to active classname
 
-   for(let i = 0; i < anchors.length; i++) {// This is the for loop and it will loop through the anchors
-      anchors[i].addEventListener('click', (event) => {// This is the eventlistner and when the click is pressed by user the event will listen for the click.
-   const li_list = ul.getElementsByTagName('li');// This is get the all elements by pulling the Tag name li
+   for (let i = 0; i < anchors.length; i++) {// This is the for loop and it will loop through the anchors
+      anchors[i].addEventListener('click', (event) => {// This is the event listener and when the click is pressed by user the event will listen for the click.
+         const li_list = ul.getElementsByTagName('li');// This is get the all elements by pulling the Tag name li
 
-   for(let i = 0; i < li_list.length; i++) {// This is the for loop and it will loop through the list
-    li_list.item(i).firstElementChild.className = "";//This is the firstElement child (li_list.item)
+         for (let i = 0; i < li_list.length; i++) {// This is the for loop and it will loop through the list
+            li_list.item(i).firstElementChild.className = "";//This is the firstElement child (li_list.item)
+         }
+         event.target.className = "active";// set event target class name to active
+         showPage(list, event.target.textContent);//This is the showPage function and it worked
+      })
    }
-      event.target.className = "active";// set event target classname to active
-      showPage(list, event.target.textContent);//This is the showPage function and it worked
-   })
-}
 }
 
 
@@ -101,20 +101,20 @@ const search = document.querySelector('#search-input');
 const searchButton = document.querySelector('#search-button');
 search.setAttribute('onkeyup', 'searchStudents(search, studentsList)');
 search.addEventListener('keyup', () => {
-   if(!(document.querySelectorAll('.match').length) && search.value !== ""){
-   document.querySelector('#none-found').style.display = "";
-   document.querySelector('.pagination').remove();
-   appendPageLinks('<ul></ul>');
-   } else if(document.querySelectorAll('.match').length) {
+   if (!(document.querySelectorAll('.match').length) && search.value !== "") {
+      document.querySelector('#none-found').style.display = "";
+      document.querySelector('.pagination').remove();
+      appendPageLinks('<ul></ul>');
+   } else if (document.querySelectorAll('.match').length) {
       document.querySelector('#none-found').style.display = "none";
       document.querySelector('.pagination').remove();
       let gotMatch = document.querySelectorAll('.match');
-      for(let i = 0; i < gotMatch.length; i++) {
-        gotMatch[i].style.display = "";
+      for (let i = 0; i < gotMatch.length; i++) {
+         gotMatch[i].style.display = "";
       }
       appendPageLinks(gotMatch);
-      showPage(gotMatch, 1);     
-   } else if(search.value == "") {
+      showPage(gotMatch, 1);
+   } else if (search.value == "") {
       document.querySelector('.pagination').remove();
       appendPageLinks(studentsList);
       showPage(studentsList, 1);
@@ -124,18 +124,18 @@ search.addEventListener('keyup', () => {
 searchButton.setAttribute('click', 'searchStudents(search, studentsList)');
 
 function searchStudents(search, students) {
-   for(let i = 0; i < students.length; i++) {
+   for (let i = 0; i < students.length; i++) {
       students[i].classList.remove('match');
-    if(search.value.length !== 0 && students[i].querySelector('h3').textContent.toLowerCase().includes(search.value.toLowerCase())) {
-    students[i].classList.add('match'); 
-    } else if(search.value.length === 0) {
-      students[i].style.display = "";
-   } else {
-       students[i].style.display = "none";
+      if (search.value.length !== 0 && students[i].querySelector('h3').textContent.toLowerCase().includes(search.value.toLowerCase())) {
+         students[i].classList.add('match');
+      } else if (search.value.length === 0) {
+         students[i].style.display = "";
+      } else {
+         students[i].style.display = "none";
       }
-    }
-  }
-   
+   }
+}
+
 //Calling all functions
 appendPageLinks(studentsList);
 showPage(studentsList, 1);
